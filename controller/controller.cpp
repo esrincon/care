23267360467025aa2cf139b8d0c9424bfdb5f864
src2 <ariel.bachman@gmail.com>
2025-05-27 17:44:42 -0700

    updtae contoller

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -19,11 +19,11 @@
  
  #include <signal.h>
  bool runloop = false;
- void sighandler(int){runloop = false;}
+ void sighandler(int) {runloop = false;}
  
  #include "redis_keys.h"
 
- bool simulation = true;
+ bool simulation = false;
 
  bool reached_vel = false;
  
@@ -40,20 +40,20 @@
 
  int prev_state = -1;
 
-// Helper: Fetch and convert all scrub points from Redis into a vector of Vector3d
-vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
-    vector<Vector3d> points;
-    vector<string> scrub_points_str = redis_client.getList(key);
+#include <sstream>   // for istringstream
 
-    for (const auto& s : scrub_points_str) {
-        istringstream ss(s);
-        double x, y, z;
-        ss >> x >> y >> z;
-        points.emplace_back(x, y, z);
-    }
-
-    return points;
-}
+// vector<Vector3d> getScrubPoints(SaiCommon::RedisClient& redis_client,
+//                                 const string& key) {
+//     vector<Vector3d> points;
+//     auto scrub_points_str = redis_client.lrange(key, 0L, -1L);
+//     for (auto const& s : scrub_points_str) {
+//         istringstream ss(s);
+//         double x, y, z;
+//         ss >> x >> y >> z;
+//         points.emplace_back(x, y, z);
+//     }
+//     return points;
+// }
 
 
  int main() {
@@ -125,9 +125,15 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 
 	 Vector3d ee_pos_clean_start;
 
-	 // get list of scrub points
-	 vector<Vector3d> scrub_points = getScrubPoints(redis_client, SCRUB_POINTS_KEY);
-	 cout << "scrub points: " << scrub_points << endl;
+	 Vector3d ee_pos_desired;
+
+	// get list of scrub points
+	// auto scrub_points = getScrubPoints(redis_client, SCRUB_POINTS_KEY);
+
+	// cout << "scrub points:" << endl;
+	// for (auto const& point : scrub_points) {
+	// 	cout << point.transpose() << endl;
+	// }
  
 	 // create a loop timer
 	 runloop = true;
@@ -159,7 +165,7 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 				 pose_task->reInitializeTask();
 				 joint_task->reInitializeTask();
  
-				 state = STOP;
+				 state = INITIAL_ROTATION;
 			 }
  
 		 } else if (state == INITIAL_ROTATION) {
@@ -199,18 +205,19 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 			 // update goal position and orientation
 
 			 // 1) select a point from list of scrub_pooints 
-			 size_t scrub_index = 2;
-			 if (scrub_points.size() > scrub_index) {
-				double offset = 0.20;
-				Vector3d ee_pos_desired = scrub_points[scrub_index];
-				ee_pos_desired(1) += offset; // Add offset to y position
-				pose_task->setGoalPosition(ee_pos_desired);
-			 } else {
-				cout << "[controller] ⚠️ Not enough scrub points available!" << endl;
-			 }
-
+			//  size_t scrub_index = 2;
+			//  if (scrub_points.size() > scrub_index) {
+			double offset = 0.20;
+			// 	ee_pos_desired = scrub_points[scrub_index];
+			// 	ee_pos_desired(1) += offset; // Add offset to y position
+			// 	pose_task->setGoalPosition(ee_pos_desired);
+			//  } else {
+			// 	cout << "[controller] ⚠️ Not enough scrub points available!" << endl;
+			//  }
+			ee_pos_desired << 0.482223, -0.081536 + offset, 0.328810;
+			pose_task->setGoalPosition(ee_pos_desired);
 			 // 2) turn on velocity saturation (linear , angular )
-			 pose_task->enableVelocitySaturation(0.1, 0.5);
+			pose_task->enableVelocitySaturation(0.1, 0.5);
  
 			 // 3) build your task hierarchy as usual
 			 N_prec.setIdentity();
@@ -240,8 +247,18 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 
 			} else if (state == INITIAL_APPROACH2) {
 				double desired_y_vel = -0.05;
-				pose_task->setPosControlGains(0.0, 20.0, 0.0);
+
+				VectorXd kp_gain(3);
+			    kp_gain << 100.0, 0.0, 100.0;
+				VectorXd kv_gain = VectorXd::Constant(3, 30.0);
+			    VectorXd ki_gain = VectorXd::Zero(3);
+				pose_task->setPosControlGains(kp_gain, kv_gain, ki_gain);
+				
 				pose_task->setGoalLinearVelocity(Eigen::Vector3d(0.0, desired_y_vel, 0.0));
+
+				ee_pos_desired << 0.482223, -0.081536 , 0.328810;
+				pose_task->setGoalPosition(ee_pos_desired);
+
 
 				N_prec.setIdentity();
 				pose_task->updateTaskModel(N_prec);
@@ -281,14 +298,14 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 			 double t_elapsed = time - clean1_start_time;
  
 			 // get these points from redis keys
-			 const double torso_center_z    = 0.492;     // from <origin xyz=…>
-			 const double torso_half_height = 0.3 / 2; // box size z = 0.5
+			 const double torso_center_z    = ee_pos_clean_start[2];     // from <origin xyz=…>
+			 const double torso_half_height = 0.2 / 2; // box size z = 0.5
  
 			 // ■ compute amplitude so sponge never crosses top/bottom
-			 double amplitude_z = torso_half_height/2;
+			 double amplitude_z = torso_half_height;
  
 			 // ■ pick a frequency (e.g. 0.2 Hz ⇒ one full up+down every 5 s)
-			 const double freq1  = .20;
+			 const double freq1  = 0.01;
 			 double omega = 2.0 * M_PI * freq1;
  
 			 // ■ desired Z = center + amplitude * sin(ω t)
@@ -297,24 +314,28 @@ vector<Vector3d> getScrubPoints(RedisClient& redis_client, const string& key) {
 			//  double zdotdot_des = -amplitude_z * sin(omega * t_elapsed) * omega * omega;
 
 	 		double y_des = ee_pos_clean_start[1] - 0.01;
+			double x_des = ee_pos_clean_start[0];
  
 			 // ■ build the full 3D goal (keep your contact Y fixed)
+			 // Karim: We were using this ee_cur but don't think we need it. Leads to drift
 			 Vector3d ee_cur_pos = robot->position(control_link, control_point);
 			 Vector3d ee_cur_vel = robot->linearVelocity(control_link, control_point);
 			 Vector3d goal;
-			 goal << ee_cur_pos.x(), ee_cur_pos.y(), z_des;
+			 goal << x_des, y_des, z_des;
 
-			VectorXd kp_gain_orientation(3);
+			 VectorXd kp_gain_orientation(3);
 			 kp_gain_orientation << 10.0, 100.0, 10.0;
 			 VectorXd kv_gain_orientation = VectorXd::Constant(3, 10.0);
 			 VectorXd ki_gain_orientation = VectorXd::Zero(3);
 			 pose_task->setOriControlGains(kp_gain_orientation, kv_gain_orientation, ki_gain_orientation);
 
 			 VectorXd kp_gain(3);
-			 kp_gain << 100.0, 10.0, 100.0;
-			 VectorXd kv_gain = VectorXd::Constant(3, 10.0);
+			 kp_gain << 100.0, 0.0, 100.0;
+			 VectorXd kv_gain = VectorXd::Constant(3, 20.0);
 			 VectorXd ki_gain = VectorXd::Zero(3);
 			 pose_task->setPosControlGains(kp_gain, kv_gain, ki_gain);
+			 double des_y_vel = -0.025;
+			 pose_task->setGoalLinearVelocity(Eigen::Vector3d(0.0, des_y_vel, 0.0));
 			 pose_task->setGoalPosition(goal);
 			 //pose_task->setGoalLinearVelocity(Eigen::Vector3d(ee_cur_vel.x(), ee_cur_vel.y(), zdot_des));
 			 //pose_task->setGoalLinearAcceleration(Vector3d(0.0, 0.0, zdotdot_des));
